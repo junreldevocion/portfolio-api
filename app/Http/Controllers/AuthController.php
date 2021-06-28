29858d52a235c221abb07ee1d92a587623041e25
Junrel <junrel.mdevocion@gmail.com>
2021-06-28 12:44:24 +0800

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request) {
+        
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(! $user || ! Hash::check($fields['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'message' => ['Bad Creds']
+            ]);
+        }
+
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function logout(Request $request) {
+
+        auth()->user()->tokens()->delete();
+
+        return response(['message' => 'Logged out']);
+
+    } 
+}
